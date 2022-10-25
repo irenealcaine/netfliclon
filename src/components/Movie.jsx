5f54a8b3a762a366769b5bb62cbc593d406eec53
Irene Alcaine <irenealcainealvarez@gmail.com>
2022-10-25 23:47:07 +0200

    added favicon

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -14,7 +14,7 @@ const Movie = ({ item }) => {
   return (
     <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
       <img
-        src={`https://image.tmdb.org/t/p/w500${item?.backdrop_path}` || placeholder}
+        src={`https://image.tmdb.org/t/p/w500${item?.backdrop_path}`}
         placeholder='blur'
         alt={item?.title}
         className='w-full h-auto block'
