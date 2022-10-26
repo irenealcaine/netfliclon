6b2ab5fe6f144410f99935ba8c2a1a4706d92b42
Irene Alcaine <irenealcainealvarez@gmail.com>
2022-10-26 22:01:34 +0200

    added firebase

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import { AuthContextProvider } from "./context/AuthContext";
+import Account from "./pages/Account";
 import Home from "./pages/Home";
+import LogIn from "./pages/LogIn";
+import SignUp from "./pages/SignUp";
 
 
 function App() {
   return (
     <>
-      <NavBar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
+      <AuthContextProvider>
+        <NavBar />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/login' element={<LogIn />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/account' element={<Account />} />
+        </Routes>
+      </AuthContextProvider>
     </>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
     <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
-      <h1 className='text-red-600 text-4xl font-black uppercase cursor-pointer'>Netfliclon</h1>
+      <Link to='/'>
+        <h1 className='text-red-600 text-4xl font-black uppercase cursor-pointer'>Netfliclon</h1>
+      </Link>
       <div>
-        <button className=' pr-4 cursor-pointer text-white'>Iniciar sesión</button>
-        <button className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white'>Registrarse</button>
+        <Link to='/login'>
+          <button className=' pr-4 cursor-pointer text-white'>Iniciar sesión</button>
+        </Link>
+        <Link to='/signup'>
+          <button className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white'>Registrarse</button>
+        </Link>
+
       </div>
     </div >
   )
