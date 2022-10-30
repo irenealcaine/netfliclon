f847106eafcc3b08dbcbd67521fe998c4b3ee5d6
Irene Alcaine <irenealcainealvarez@gmail.com>
2022-10-30 16:56:46 +0100

    netlify

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -5,8 +5,8 @@ const Account = () => {
   return (
     <>
       <div className="w-full text-white">
-        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/79fe83d4-7ef6-4181-9439-46db72599559/33195bfc-7f65-4308-8869-677efc451cad/ES-es-20221017-popsignuptwoweeks-perspective_alpha_website_small.jpg" alt="" className='w-full h-[300px] object-cover' />
-        <div className="bg-black/60 fixed top-0 w-full h-[300px]"></div>
+        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/79fe83d4-7ef6-4181-9439-46db72599559/33195bfc-7f65-4308-8869-677efc451cad/ES-es-20221017-popsignuptwoweeks-perspective_alpha_website_small.jpg" alt="" className='w-full h-[280px] object-cover' />
+        <div className="bg-black/60 fixed top-0 w-full h-[280px]"></div>
         <div className="absolute top-[20%] p-4 md:p-8">
           <h1 className="text-3xl md:text-5xl font-bold">Mi cuenta</h1>
         </div>
